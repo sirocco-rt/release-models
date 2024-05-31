90d20027387806275b749b58ea0612913e94d2bf
jhmatthews <matthews@ast.cam.ac.uk>
2024-05-31 13:43:23 +0100

    don't use tex in github workflow

--- Plot/make_all_figures.py
+++ Plot/make_all_figures.py
@@ -1,11 +1,12 @@
 import plot_tde, tardis_fig, plot_parallel_scaling
 import plot_cv, plot_quasar
+import sys 
 
 tex = "True"
 if len(sys.argv) > 1:
     if sys.argv[1] == "--notex":
         tex = "False"
-        
+
 util.set_plot_defaults(tex=tex)
 tardis_fig.make_figure()
 plot_parallel_scaling.make_figure()
